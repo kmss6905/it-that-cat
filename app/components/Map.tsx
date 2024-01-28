548da89b6b8694fd999f1849af9bdc92ff3535f7
catca <kdg96g@gmail.com>
2024-01-28 21:04:38 +0900

    feat: 카카오 맵 추가

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,21 +1,58 @@
 'use client';
-import getAddress, { RegionState } from '@/apis/map/getAddress';
+
 import useGeolocation from '@/hooks/useGeolocation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 const Map = () => {
   const geolocation = useGeolocation();
-  const [region, setRegion] = useState<undefined | RegionState>();
 
   useEffect(() => {
-    if (geolocation.position) {
-      getAddress(geolocation.position).then((address) => {
-        setRegion(address);
-      });
+    const mapScript = document.createElement('script');
+    if (geolocation.position === null) {
+      return;
     }
-  }, [geolocation]);
+    const { latitude, longitude } = geolocation.position;
 
-  return <div>Map</div>;
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(latitude, longitude),
+        };
+        const map = new window.kakao.maps.Map(container, options);
+        const markerPosition = new window.kakao.maps.LatLng(
+          latitude,
+          longitude,
+        );
+        const nowMarker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        nowMarker.setMap(map);
+      });
+    };
+    mapScript.addEventListener('load', onLoadKakaoMap);
+
+    return () => mapScript.removeEventListener('load', onLoadKakaoMap);
+  }, [geolocation.position]);
+
+  return (
+    <div
+      id='map'
+      className='w-full'
+      style={{ height: 'calc(812px - 120px)' }}
+    />
+  );
 };
 
 export default Map;
