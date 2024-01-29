14dcad895a8fc39b57ead9fb0c3b51d9ec5749cb
Dorabang <psa1195@naver.com>
2024-01-29 19:22:34 +0900

    feat: 컬러 시스템 등록 및 툴팁 애니메이션 등록

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          100: '#FFF3E1',
+          200: '#FFE0B5',
+          300: '#FFCD85',
+          400: '#FFB14A',
+          500: '#FF9B1E',
+        },
+        gray: {
+          50: '#F2F2F2',
+          100: '#DDDDDD',
+          200: '#B7B7B7',
+          300: '#909090',
+          400: '#6D6D6D',
+          500: '#4D4D4D',
+        },
+        black: '#2B2B2B',
       },
+      keyframes: {
+        tooltip: {
+          '0%': { opacity: '0' },
+          '40%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: { tooltip: 'tooltip 1s ease-in-out' },
     },
   },
   plugins: [],
