2e5a479a3b79f137961368e14983f6a6abeca2f5
Dorabang <psa1195@naver.com>
2024-01-30 02:24:47 +0900

    design: hover css 수정

--- app/components/Tooltip/index.tsx
+++ app/components/Tooltip/index.tsx
@@ -9,11 +9,13 @@ interface TootipProps {
 const Tooltip = ({ children, message, direction = 'top' }: TootipProps) => {
   return (
     <div className={`tooltipWrapper`}>
-      {children}
+      <span className='w-4 h-4 flex justify-center items-center border border-gray-300 rounded-full'>
+        {children}
+      </span>
       <div
         className={`tooltip
-        ${direction === 'top' ? 'before:block absolute -left-11 -top-16 z-50' : ''}
-        ${direction === 'bottom' ? 'after:block absolute -left-11 -bottom-16 z-50' : ''}`}
+        ${direction === 'top' ? 'before:block absolute -left-11 -top-16' : ''}
+        ${direction === 'bottom' ? 'after:block absolute -left-11 -bottom-16' : ''}`}
       >
         {message}
       </div>
