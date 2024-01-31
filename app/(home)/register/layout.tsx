4d2190e6c1e0fee024b026b744a8b34eae077b11
catca <kdg96g@gmail.com>
2024-02-01 01:18:33 +0900

    design: layout 스크롤 안 보이게 수정

--- app/(home)/register/layout.tsx
+++ app/(home)/register/layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 
 const RegisterLayout = ({ children }: { children: ReactNode }) => {
-  return <div className='h-full overflow-y-scroll'>{children}</div>;
+  return <div className='layout h-full overflow-y-scroll'>{children}</div>;
 };
 
 export default RegisterLayout;
