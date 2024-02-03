6cc6d50e5de382e5e0c23a0dd72f27d2f53a58c1
Dorabang <psa1195@naver.com>
2024-02-03 22:48:08 +0900

    build: 카카오 redirectURI 변경

--- app/components/Home/CardSkeleton.tsx
+++ app/components/Home/CardSkeleton.tsx
@@ -1,6 +1,6 @@
 const CardSkeleton = () => {
   return (
-    <div className='flex gap-3 w-full h-full px-4 py-5'>
+    <div className='flex gap-3 w-full h-full px-4 py-5 bg-white'>
       <div className='w-[70px] h-[70px] rounded-full animate-pulse bg-gray-100'></div>
       <div className='flex-grow flex flex-col justify-between'>
         <div className='flex flex-col gap-2'>
