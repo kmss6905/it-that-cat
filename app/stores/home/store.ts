8552bbc94d826f807d6cca54c04c265e40072e19
Dorabang <psa1195@naver.com>
2024-02-29 16:48:21 +0900

    refactor: 불필요한 코드 제거

--- app/stores/home/store.ts
+++ app/stores/home/store.ts
@@ -12,37 +12,22 @@ type GeolocationStore = {
   setLevel: (level: number) => void;
   setPosition: (position: Coordinates) => void;
   setAddress: (addr: RegionState) => void;
-  setEmpty: () => void;
 };
 
 const geolocationStore: StateCreator<GeolocationStore> = (set) => ({
   geolocation: { level: 3, position: null, address: null },
   setLevel: (level) =>
     set((prev) => ({
-      geolocation: {
-        level: level,
-        position: prev.geolocation.position,
-        address: prev.geolocation.address,
-      },
+      geolocation: { ...prev.geolocation, level: level },
     })),
   setPosition: (position) =>
     set((prev) => ({
-      geolocation: {
-        level: prev.geolocation.level,
-        address: prev.geolocation.address,
-        position: position,
-      },
+      geolocation: { ...prev.geolocation, position: position },
     })),
   setAddress: (addr) =>
     set((prev) => ({
-      geolocation: {
-        level: prev.geolocation.level,
-        position: prev.geolocation.position,
-        address: addr,
-      },
+      geolocation: { ...prev.geolocation, address: addr },
     })),
-  setEmpty: () =>
-    set(() => ({ geolocation: { level: 3, position: null, address: null } })),
 });
 
 export const useGeolocationStore = create(geolocationStore);
