2e560b2297e832692e18efb8f29cf634d3f53aad
Dorabang <psa1195@naver.com>
2024-04-24 16:59:53 +0900

    feat: 회원탈퇴 모달

---
+++ app/components/MyPage/DeleteUserModal/indext.tsx
@@ -0,0 +1,41 @@
+import Modal, { MODAL_TYPE, MODAL_VARIANT } from '@/components/Modal';
+import ImgDeleteUser from '@/assets/images/mypage/img_deleteUser.svg';
+import { useModal } from '@/hooks/useModal';
+
+const DeleteUserModal = () => {
+  const { closeModal } = useModal();
+
+  const handleDeleteUser = async () => {};
+
+  return (
+    <Modal type={MODAL_TYPE.DELETE_USER} variant={MODAL_VARIANT.CARD}>
+      <div className='flex flex-col gap-5 pt-11 pb-10 items-center text-center [&_h3]:subHeading [&_p]:body2 text-gray-500'>
+        <ImgDeleteUser />
+        <div>
+          <h3 className='pb-2'>정말로 회원을 탈퇴하시겠어요?</h3>
+          <p>
+            회원 탈퇴 시 계정 정보 및 작성한 게시글과
+            <br />
+            댓글은 모두 삭제되어 복구가 불가능해요.
+          </p>
+        </div>
+      </div>
+      <div className='border-t border-gray-100 w-full subHeading'>
+        <button
+          onClick={closeModal}
+          className='w-1/2 py-4 text-center hover:bg-gray-50 active:bg-gray-50 text-black border-r border-gray-100'
+        >
+          취소
+        </button>
+        <button
+          onClick={() => handleDeleteUser()}
+          className='w-1/2 py-4 text-center hover:bg-gray-50 active:bg-gray-50 text-error'
+        >
+          회원탈퇴
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteUserModal;
