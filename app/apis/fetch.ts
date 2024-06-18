ca7458dee1e515db161297f3469acab73d268eca
Dorabang <psa1195@naver.com>
2024-06-18 22:16:20 +0900

    rafactor: 서버 컴포넌트 사용하지 않고 기본 설정 추가할 수 있도록 수정

--- app/apis/fetch.ts
+++ app/apis/fetch.ts
@@ -1,25 +1,15 @@
-'use server';
-import { cookies } from 'next/headers';
 import returnFetch from 'return-fetch';
-
-const cookieStore = cookies();
-const accessToken = cookieStore.get('accessToken');
 
 const base =
   process.env.NODE_ENV === 'production'
     ? process.env.NEXT_PUBLIC_SERVER_URL
     : process.env.NEXT_PUBLIC_DEV_SERVER_URL;
+
 /**
  * baseUrl, headers 추가하여 확장된 fetch
  */
 const fetchExtended = returnFetch({
   baseUrl: base,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + `${accessToken?.value}`,
-  },
 });
 
 export default fetchExtended;
