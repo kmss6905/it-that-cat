3a892c01e537783ffb12db66383ac1f68e55513e
catca <kdg96g@gmail.com>
2024-06-20 02:31:38 +0900

    feat: 로딩 시 lottie json 파일 적용

--- app/components/Loading/index.tsx
+++ app/components/Loading/index.tsx
@@ -1,13 +1,16 @@
 'use client';
 import { useLoading } from '@/stores/loading/store';
+import Lottie from 'react-lottie-player';
+
+import loadingLottie from '@/assets/images/lottie/loading.json';
 
 const Loading = () => {
   const { loading } = useLoading();
 
   if (loading)
     return (
-      <div className='fixed top-0 left-1/2 flex justify-center items-center h-screen'>
-        <div className='animate-spin rounded-full w-10 h-10 border-4 border-solid border-black/80 border-l-black'></div>
+      <div className='fixed w-full top-0 left-1/2 -translate-x-1/2 flex justify-center items-center h-screen bg-white/60 z-40 px-20'>
+        <Lottie loop animationData={loadingLottie} play />
       </div>
     );
   return null;
