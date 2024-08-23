6e48f26a9e4a5d0b2df76fff0fcc62d46cdc5a87
Dorabang <psa1195@naver.com>
2024-08-23 17:16:37 +0900

    feat: 주소 검색 기능 추가
    - 최근 검색어 저장 기능 추가(sessionStorage 관리)
    - 추천 검색어 기능 추가(주소 리스트를 api로 받아와 검색어와 유사한 추천 검색어 리스트를 추리는 방식으로 진행, fuzzySearch 참고)
    - 검색 api 연결 후, 무한 스크롤 적용

--- app/components/Home/Search/SearchModal/index.tsx
+++ app/components/Home/Search/SearchModal/index.tsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useEffect, useMemo, useState } from 'react';
+
 import IconBack from '@/assets/images/icon_back_black.svg';
 import IconGraySearch from '@/assets/images/icon_graySearch.svg';
 import IconGraySearchM from '@/assets/images/icon_graySearchM.svg';
 import IconRemoveValue from '@/assets/images/Icon_removeValue.svg';
+import IconRemoveSearch from '@/assets/images/search/icon-removeSearch.svg';
 import Modal, { MODAL_TYPE, MODAL_VARIANT } from '@/components/Modal';
-import { useModal } from '@/hooks/useModal';
-import useDebounceFunction from '@/hooks/utils/useDebounceFunction';
 import { highlight } from '@/utils/highlight';
 import getAdmAddr, { AdmAddrData } from '@/utils/getAdmAddr';
+import { useModal } from '@/hooks/useModal';
+import useDebounceFunction from '@/hooks/utils/useDebounceFunction';
+import useGeolocation from '@/hooks/useGeolocation';
+import { useSearch } from '@/hooks/queries/useSearch';
+import SearchResultCard from './SearchResultCard';
+import { SearchContentProps } from '@/types/content';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 
 export interface QueryState {
   key: string;
   data: AdmAddrData[];
 }
 
-export interface SearchState {
-  admAddr: AdmAddrData;
-  data: AdmAddrData[] | null;
-}
-
 const SearchModal = () => {
   const { closeModal } = useModal();
+  const { position } = useGeolocation();
 
   const [query, setQuery] = useState<QueryState>({ key: '', data: [] });
-  const [search, setSearch] = useState<SearchState>({
-    admAddr: {
-      sidoCode: 0,
-      sidoName: '',
-      guCode: 0,
-      siGunGuName: '',
-      dongCode: 0,
-      dongName: '',
-      fullAddr: '',
-    },
-    data: null,
-  });
+  const [search, setSearch] = useState<string | null>(null);
+  const [recentSearch, setRecentSearch] = useState<string[] | null>(null);
+
+  useEffect(() => {
+    const strRecentSearch = sessionStorage.getItem('recentSearch');
+    const recentSearchList = strRecentSearch ? JSON.parse(strRecentSearch) : [];
+
+    setRecentSearch(recentSearchList);
+  }, []);
 
   const getPlaceList = async (search: string) => {
     const response = await getAdmAddr(search);
@@ -44,18 +44,59 @@ const SearchModal = () => {
   const setDebouncePlaceList = useDebounceFunction(getPlaceList, 300);
 
   const handleChangeSearchValue = (value: string) => {
+    setSearch(null);
     setQuery((prev) => ({ ...prev, key: value }));
     setDebouncePlaceList(value);
   };
 
+  const { data, hasNextPage, isFetching, fetchNextPage } = useSearch(
+    position,
+    search,
+  );
+
+  const target = useIntersectionObserver((entry, observer) => {
+    observer.unobserve(entry.target);
+
+    if (hasNextPage && !isFetching) fetchNextPage();
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(query.key);
+
+    const updateRecentSearch = recentSearch
+      ? [...recentSearch, query.key]
+      : [query.key];
+    setRecentSearch(updateRecentSearch);
+    sessionStorage.setItem('recentSearch', JSON.stringify(updateRecentSearch));
+  };
+
+  const result = useMemo(() => {
+    if (data) {
+      const searchResult = data.pages.flatMap((list) =>
+        list ? list.items : [],
+      );
+
+      return searchResult;
+    }
+    return null;
+  }, [data]);
+
   return (
     <Modal type={MODAL_TYPE.SEARCH} variant={MODAL_VARIANT.ALL}>
-      <div className='w-full h-full bg-white z-30 relative'>
+      <div className='w-full h-full bg-white'>
         <form
-          onSubmit={(e) => e.preventDefault()}
+          onSubmit={(e) => handleSubmit(e)}
           className={`w-full pl-5 pr-6 pb-10px pt-18px flex justify-between items-center gap-3`}
         >
-          <div onClick={() => closeModal()} className='cursor-pointer'>
+          <div
+            onClick={() => {
+              closeModal();
+              setSearch(null);
+              setQuery({ data: [], key: '' });
+            }}
+            className='cursor-pointer'
+          >
             <IconBack />
           </div>
 
@@ -72,7 +113,10 @@ const SearchModal = () => {
             {query.key !== '' && (
               <div
                 className='cursor-pointer'
-                onClick={() => setQuery((prev) => ({ ...prev, key: '' }))}
+                onClick={() => {
+                  setQuery((prev) => ({ ...prev, key: '' }));
+                  setSearch(null);
+                }}
               >
                 <IconRemoveValue />
               </div>
@@ -80,18 +124,78 @@ const SearchModal = () => {
           </div>
         </form>
 
+        {recentSearch && query.key === '' && search === null && (
+          <ul className='px-6 py-5 flex flex-col gap-1 h-[calc(100%-128px)] overflow-y-scroll layout'>
+            {recentSearch.length > 0 ? (
+              <>
+                <li className='flex justify-between items-center pt-5 pb-3'>
+                  <h3 className='subHeading text-gray-500'>최근 검색</h3>
+                  <button
+                    className='caption text-gray-300'
+                    onClick={() => {
+                      sessionStorage.removeItem('recentSearch');
+                      setRecentSearch([]);
+                    }}
+                  >
+                    모두 지우기
+                  </button>
+                </li>
+                {recentSearch.map((list) => (
+                  <li
+                    key={list}
+                    className='flex gap-3 justify-between items-center py-2'
+                  >
+                    <button
+                      className='body2 text-gray-500 flex-grow text-left'
+                      onClick={() => {
+                        setSearch(list);
+                      }}
+                    >
+                      {list}
+                    </button>
+                    <button
+                      onClick={() => {
+                        const removeList = recentSearch.filter(
+                          (item) => item !== list,
+                        );
+                        setRecentSearch(removeList);
+                        sessionStorage.setItem(
+                          'recentSearch',
+                          JSON.stringify(removeList),
+                        );
+                      }}
+                    >
+                      <IconRemoveSearch />
+                    </button>
+                  </li>
+                ))}
+              </>
+            ) : (
+              <li className='py-[72px] text-center body2 text-gray-400'>
+                최근 검색한 내역이 없습니다.
+              </li>
+            )}
+          </ul>
+        )}
+
         {/* 검색어 추천 */}
-        {query.key !== '' && query.data?.length > 0 && search.data === null ? (
-          <ul className='px-6 py-5 flex flex-col gap-1'>
+        {query.key !== '' && query.data?.length > 0 && search === null ? (
+          <ul className='px-6 py-5 flex flex-col gap-1 h-[calc(100%-128px)] overflow-y-scroll layout'>
             {query.data.map((place) => (
               <li
                 key={place.fullAddr}
                 onClick={() => {
-                  setSearch({
-                    admAddr: place,
-                    data: [],
-                  });
+                  setSearch(place.fullAddr);
                   setQuery((prev) => ({ ...prev, key: place.fullAddr }));
+
+                  const updateRecentSearch = recentSearch
+                    ? [...recentSearch, place.fullAddr]
+                    : [place.fullAddr];
+                  setRecentSearch(updateRecentSearch);
+                  sessionStorage.setItem(
+                    'recentSearch',
+                    JSON.stringify(updateRecentSearch),
+                  );
                 }}
                 className='flex justify-between items-center gap-[10px] text-gray-500 cursor-pointer'
               >
@@ -103,6 +207,35 @@ const SearchModal = () => {
             ))}
           </ul>
         ) : null}
+
+        {result &&
+          query.key !== '' &&
+          (result.length > 0 ? (
+            <>
+              <h3 className='subHeading text-gray-500 px-6 pt-5 pb-3'>
+                검색 결과
+              </h3>
+              <ul className='px-6 h-[calc(100%-128px)] overflow-y-scroll layout'>
+                {result.map((result) => (
+                  <li key={result?.contentId} onClick={() => closeModal()}>
+                    <SearchResultCard result={result} />
+                  </li>
+                ))}
+                <div ref={target} />
+              </ul>
+            </>
+          ) : (
+            /* 검색 결과가 없을 시 */
+            <div className='py-16 text-center'>
+              <h3 className='body2 text-gray-400 pb-1'>
+                <span className='subHeading2 text-primary-500'>'{search}'</span>
+                에 등록된 냥이가 없습니다.
+              </h3>
+              <p className='caption text-gray-300'>
+                직접 등록하거나 주변의 다른 동네를 검색해보세요!
+              </p>
+            </div>
+          ))}
       </div>
     </Modal>
   );
